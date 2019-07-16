a22fb5de875c79a01b053fa5237bf179f186c00f
adrian <adrianmatyszczak@gmail.com>
2019-07-16 22:23:02 +0200

    Implemented ClientCreated Event

--- clientservices/src/main/kotlin/pl/edu/pollub/virtualcasino/clientservices/domain/client/events/ClientCreated.kt
+++ clientservices/src/main/kotlin/pl/edu/pollub/virtualcasino/clientservices/domain/client/events/ClientCreated.kt
@@ -1,23 +1,24 @@
 package pl.edu.pollub.virtualcasino.clientservices.domain.client.events
 
 import pl.edu.pollub.virtualcasino.clientservices.domain.DomainEvent
+import pl.edu.pollub.virtualcasino.clientservices.domain.client.ClientId
 import java.time.Instant
 import java.util.UUID.randomUUID
 
-data class UserCreated(
-        val id: UserCreatedId = UserCreatedId()
+data class ClientCreated(
+        val id: UserCreatedId = UserCreatedId(),
+        val clientId: ClientId,
+        val occurredAt: Instant = Instant.now()
 ): DomainEvent {
 
-    override fun type(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun type(): String = TYPE
 
-    override fun occurredAt(): Instant {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun occurredAt(): Instant = occurredAt
 
-    override fun aggregateUuid(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun aggregateUuid(): String = clientId.value
+
+    companion object {
+        const val TYPE = "client.clientCreated"
     }
 }
 
