18df1d901a9a73a0e92172db68b23de4f9076853
adrian <adrianmatyszczak@gmail.com>
2019-07-23 21:00:26 +0200

    Added SampleClient event

--- clientservices/src/test/groovy/pl/edu/pollub/virtualcasino/clientservices/domain/client/samples/SampleClientCreated.groovy
+++ clientservices/src/test/groovy/pl/edu/pollub/virtualcasino/clientservices/domain/client/samples/SampleClientCreated.groovy
@@ -1,4 +1,31 @@
 package pl.edu.pollub.virtualcasino.clientservices.domain.client.samples
 
+import pl.edu.pollub.virtualcasino.clientservices.domain.client.ClientId
+import pl.edu.pollub.virtualcasino.clientservices.domain.client.events.ClientCreated
+import pl.edu.pollub.virtualcasino.clientservices.domain.client.events.ClientCreatedId
+
+import java.time.Instant
+
+
 class SampleClientCreated {
+
+    static ClientCreated sampleClientCreated(customProperties = [:]) {
+        def properties = [
+                id: sampleClientCreatedId(),
+                clientId: sampleClientId(),
+                occuredAt: samplePointInTime()
+        ] + customProperties
+        return new ClientCreated(
+                properties.id as ClientCreatedId,
+                properties.clientId as ClientId,
+                properties.occuredAt as Instant
+        )
+    }
+
+    static ClientCreatedId sampleClientCreatedId(customProperties = [:]) {
+        def properties = [
+                value: randomUUID().toString()
+        ] + customProperties
+        return new ClientCreatedId(properties.value as String)
+    }
 }
