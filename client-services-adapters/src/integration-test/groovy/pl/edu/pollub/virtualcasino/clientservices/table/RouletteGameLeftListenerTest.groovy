1056b99b25b56c51c98fa0118d5204598f7e2a2a
Jarek0 <jaroslaw.bielec.1994@gmail.com>
2019-09-01 21:18:26 +0200

    VC-4 | charging client that left game

--- client-services-adapters/src/integration-test/groovy/pl/edu/pollub/virtualcasino/clientservices/table/RouletteGameLeftListenerTest.groovy
+++ client-services-adapters/src/integration-test/groovy/pl/edu/pollub/virtualcasino/clientservices/table/RouletteGameLeftListenerTest.groovy
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import pl.edu.pollub.virtualcasino.clientservices.ClientServicesApiTest
 import spock.lang.Subject
 
+import static pl.edu.pollub.virtualcasino.clientservices.client.samples.SampleTokens.sampleTokens
 import static pl.edu.pollub.virtualcasino.clientservices.table.samples.SampleTable.sampleParticipation
 import static pl.edu.pollub.virtualcasino.roulettegame.samples.SampleRouletteGameId.sampleRouletteGameId
 import static pl.edu.pollub.virtualcasino.roulettegame.samples.SampleRoulettePlayerId.sampleRoulettePlayerId
@@ -15,23 +16,27 @@ class RouletteGameLeftListenerTest extends ClientServicesApiTest {
     @Autowired
     RouletteGameLeftListener listener
 
-    def "should left table"() {
+    def "should charge client and left table"() {
         given:
             def clientThatReservedTable = setupClient()
             def reservedTableId = reserveTable(clientThatReservedTable.id())
         and:
-            def clientThatJoinedTable = setupClient()
+            def clientThatJoinedTable = setupClientWithTokens(sampleTokens(count: 50))
             joinTable(reservedTableId, clientThatJoinedTable.id())
         and:
             def gameId = sampleRouletteGameId(value: reservedTableId.value)
             def playerId = sampleRoulettePlayerId(value: clientThatJoinedTable.id().value)
-            def gameLeft = sampleRouletteGameLeft(gameId: gameId, playerId: playerId)
+            def clientThatJoinedTableTokensAfterPlaying = sampleTokens(count: 100)
+            def gameLeft = sampleRouletteGameLeft(gameId: gameId, playerId: playerId, playerTokens: clientThatJoinedTableTokensAfterPlaying)
         and:
             def participationOfClientThatReservedTable = sampleParticipation(clientId: clientThatReservedTable.id())
             def unexpectedParticipation = sampleParticipation(clientId: clientThatJoinedTable.id())
         when:
             listener.reactTo(gameLeft)
         then:
+            def clientThatLeftGame = clientRepository.find(clientThatJoinedTable.id())
+            clientThatLeftGame.tokens() == clientThatJoinedTableTokensAfterPlaying
+        and:
             def reservedTable = tableRepository.find(reservedTableId)
             reservedTable != null
             def participation = reservedTable.participation()
